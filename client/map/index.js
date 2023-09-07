50c3c604085e930823124d84540df36ca6e232b5
기다빈 <a02441@hanatour.com>
2023-09-07 16:33:51 +0900

    add map.html initCanvas

--- client/map/index.js
+++ client/map/index.js
@@ -14,4 +14,24 @@ window.addEventListener("load", init);
 function init() {
   canvas = document.getElementById("cvs");
   context = canvas.getContext("2d");
+
+  initCanvas();
+}
+
+function initCanvas() {
+  if (canvas === null) return;
+  if (context === null) return;
+
+  canvasResize();
+  window.addEventListener("resize", canvasResize);
+}
+
+function canvasResize() {
+  canvasWidth = window.innerWidth * window.devicePixelRatio;
+  canvasHeight = window.innerHeight * window.devicePixelRatio;
+
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  canvas.style.width = canvasWidth / window.devicePixelRatio + "px";
+  canvas.style.height = canvasHeight / window.devicePixelRatio + "px";
 }
